2768a8c43a72db45dedd4c190f5c5b0545c7dc88
JChamblee99 <github@proton.farm>
2025-08-04 23:03:34 -0500

    Added SSH key creation to agents CLI

--- labrat/cli/agents.py
+++ labrat/cli/agents.py
@@ -10,11 +10,16 @@ def build_parser(subparser=None):
     parser = argparse.ArgumentParser("labrat agents") if subparser is None else subparser
     
     subparsers = parser.add_subparsers(dest="command", required=True)
+
     list_parser = subparsers.add_parser("list", aliases=["ls"], help="List GitLab servers")
     build_sub_parser(handle_list_args, list_parser)
     
     delete_parser = subparsers.add_parser("delete", aliases=["rm"], help="Delete GitLab server from config")
     build_sub_parser(handle_delete_args, delete_parser)
+
+    add_key_parser = subparsers.add_parser("add-key", help="Add SSH key to the user account")
+    build_add_key_parser(add_key_parser)
+
     return parser
 
 def build_sub_parser(handle=None, subparser=None, prog=None):
@@ -22,6 +27,18 @@ def build_sub_parser(handle=None, subparser=None, prog=None):
     parser.add_argument("-a", "--all", action="store_true", required=False, help="All agents")
     parser.add_argument("-f", "--filter", required=False, help="Filter agents by substring")
     parser.set_defaults(func=handle)
+    return parser
+
+def build_add_key_parser(subparser=None, prog=None):
+    parser = argparse.ArgumentParser(prog) if subparser is None else subparser
+    parser.add_argument("-f", "--filter", required=False, help="Filter agents by substring")
+    parser.add_argument("-t", "--title", required=False, default="Default Key", help="Title for the SSH key")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-k", "--key", required=False, help="Public SSH key to add")
+    group.add_argument("-K", "--key-file", required=False, help="Path to public SSH key file")
+    
+    parser.set_defaults(func=handle_add_key_args)
     return parser
 
 def handle_list_args(args):
@@ -74,3 +91,22 @@ def handle_delete_args(args):
         print(f"[-] Deleted {section} from config")
 
     return
+
+def handle_add_key_args(args):
+    if args.key:
+        key = args.key
+    elif args.key_file:
+        with open(args.key_file, "r") as f:
+            key = f.read().strip()
+
+    config = Config()
+    
+    for section, agent in config.filter(args.filter):
+        if agent.auth():
+            try:
+                agent.add_ssh_key(args.title, key)
+                print(f"[+] Added SSH key to {section}")
+            except Exception as e:
+                print(f"[-] Failed to add SSH key to {section}: {e}")
+        else:
+            print(f"[-] Authentication failed for {section}")
